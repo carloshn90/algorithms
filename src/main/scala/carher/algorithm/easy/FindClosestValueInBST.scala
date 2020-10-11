1182d8392eafe21c1ab5754986d729f0e5ae3028
carloshn90 <carlohn90@gmail.com>
2020-10-11 16:19:26 +0100

    Refactor improve code

--- src/main/scala/carher/algorithm/easy/FindClosestValueInBST.scala
+++ src/main/scala/carher/algorithm/easy/FindClosestValueInBST.scala
@@ -10,13 +10,16 @@ object FindClosestValueInBST {
 
   @tailrec
   def findClosetValueInBst(tree: Tree[Int], target: Int, cValue: Int = 0): Int = tree match {
-    case Empty()                  => cValue
-    case Node(value, left, right) =>
-      val vDiff = target - value
-      val cDiff = target - cValue
-      val newCValue = if (Math.abs(vDiff) <= Math.abs(cDiff)) value else cValue
+    case Empty()                                      => cValue
+    case Node(value, left, _) if (target - value) < 0 =>
+      findClosetValueInBst(left, target, calculateCloset(value, target, cValue))
+    case Node(value, _, right)                        =>
+      findClosetValueInBst(right, target, calculateCloset(value, target, cValue))
+  }
 
-      if (vDiff < 0) findClosetValueInBst(left, target, newCValue)
-      else findClosetValueInBst(right, target, newCValue)
+  def calculateCloset(value: Int, target: Int, cValue: Int): Int = {
+    val vDiff = target - value
+    val cDiff = target - cValue
+    if (Math.abs(vDiff) <= Math.abs(cDiff)) value else cValue
   }
 }
